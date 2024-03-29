26c08c5a16e3135bd405da03bb8138b444828383
Aristotelis <aristotelis.liakatas@gmail.com>
2024-03-29 22:25:43 +0000

    wip: Building the scripts to work with cmake.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,14 @@
+#include "appconfig.h"
+
 #include <iostream>
 #include <filesystem>
+#include <string>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Starting " << std::filesystem::path(argv[0]).stem() << "..." << std::endl;
-
+    std::cout << "Starting " << std::filesystem::path(argv[0]).stem().string() << "..." << std::endl;
+    // report version
+    std::cout << " Version " << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << std::endl;
 
 
 
